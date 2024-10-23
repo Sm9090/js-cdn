12b08d32fdcac134859317cf8282896f53975c8d
Muhammad Sameer <sameermemon834@gmail.com>
2024-10-24 00:54:50 +0500

    2nd

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello from  javascript cdn ")
+console.log("Hello from  javascript cdn ");
+
+async function one() {
+  const fetching = await fetch("https://dummyjson.com/products")
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+  console.log(fetching, "fetching");
+}
+one()
